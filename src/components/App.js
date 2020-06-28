6660c572d8c625d9f388479988321fad75928ae7
GangjunLee <me@gangjun.dev>
2020-06-28 09:35:24 +0900

    add logo, add react-router-dom, add .prettierrc

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
-function App() {
-  return <div className="App">asdf</div>;
+const App = ({ children }) => {
+  return <div className='App'>{children}</div>
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 import './assets/main.css'
+import './index.css'
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './pages/Home'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App>
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </App>
+  </BrowserRouter>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
